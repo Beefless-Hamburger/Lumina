7dfea3547cf10ba07bb5a33c5b983fa4ee842964
Soren Gallo <slaga3@uic.edu>
2026-07-09 22:23:21 -0500

    Add deterministic heartbeat controller

---
+++ ShutdownHeartbeatController.swift
@@ -0,0 +1,83 @@
+import Foundation
+
+protocol HeartbeatTimerToken: AnyObject {
+    func invalidate()
+}
+
+@MainActor
+protocol HeartbeatScheduling: AnyObject {
+    func schedule(
+        interval: TimeInterval,
+        tolerance: TimeInterval,
+        handler: @escaping @MainActor @Sendable () -> Void
+    ) -> any HeartbeatTimerToken
+}
+
+final class FoundationHeartbeatTimerToken: HeartbeatTimerToken {
+    private let timer: Timer
+
+    init(timer: Timer) {
+        self.timer = timer
+    }
+
+    func invalidate() {
+        timer.invalidate()
+    }
+}
+
+@MainActor
+final class FoundationHeartbeatScheduler: HeartbeatScheduling {
+    func schedule(
+        interval: TimeInterval,
+        tolerance: TimeInterval,
+        handler: @escaping @MainActor @Sendable () -> Void
+    ) -> any HeartbeatTimerToken {
+        let timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { _ in
+            Task { @MainActor in
+                handler()
+            }
+        }
+        timer.tolerance = tolerance
+        return FoundationHeartbeatTimerToken(timer: timer)
+    }
+}
+
+@MainActor
+final class ShutdownHeartbeatController {
+    private let scheduler: any HeartbeatScheduling
+    private let interval: TimeInterval
+    private let tolerance: TimeInterval
+    private var timer: (any HeartbeatTimerToken)?
+
+    init(
+        scheduler: any HeartbeatScheduling = FoundationHeartbeatScheduler(),
+        interval: TimeInterval = 900,
+        tolerance: TimeInterval = 60
+    ) {
+        self.scheduler = scheduler
+        self.interval = interval
+        self.tolerance = tolerance
+    }
+
+    var isRunning: Bool {
+        timer != nil
+    }
+
+    func start(handler: @escaping @MainActor @Sendable () -> Void) {
+        guard timer == nil else { return }
+
+        timer = scheduler.schedule(interval: interval, tolerance: tolerance) { [weak self] in
+            guard self?.timer != nil else { return }
+            handler()
+        }
+    }
+
+    func stop() {
+        timer?.invalidate()
+        timer = nil
+    }
+
+    deinit {
+        timer?.invalidate()
+    }
+}
