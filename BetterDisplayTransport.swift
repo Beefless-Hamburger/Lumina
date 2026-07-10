fb715fc4896146a010799eaa04675f50a01d7ea3
Soren Gallo <slaga3@uic.edu>
2026-07-09 22:23:02 -0500

    Add structured BetterDisplay results

--- BetterDisplayTransport.swift
+++ BetterDisplayTransport.swift
@@ -1,12 +1,49 @@
 import Foundation
 
-struct BetterDisplayExecutionResult: Sendable {
+enum BetterDisplayAvailability: Sendable, Equatable {
+    case running
+    case unavailable
+    case launchFailed(String)
+    case timedOut
+    case cancelled
+
+    var isAvailable: Bool {
+        self == .running
+    }
+}
+
+enum BetterDisplayExecutionStatus: Sendable, Equatable {
+    case succeeded
+    case executableMissing
+    case launchFailed(String)
+    case nonZeroExit(Int32)
+    case timedOut
+    case cancelled
+}
+
+struct BetterDisplayExecutionResult: Sendable, Equatable {
+    let status: BetterDisplayExecutionStatus
     let output: String
     let errorOutput: String
-    let exitCode: Int32
+    let outputWasTruncated: Bool
+    let errorOutputWasTruncated: Bool
+
+    var succeeded: Bool {
+        status == .succeeded
+    }
+
+    static func failure(_ status: BetterDisplayExecutionStatus) -> BetterDisplayExecutionResult {
+        BetterDisplayExecutionResult(
+            status: status,
+            output: "",
+            errorOutput: "",
+            outputWasTruncated: false,
+            errorOutputWasTruncated: false
+        )
+    }
 }
 
 protocol BetterDisplayTransport: Sendable {
-    func ensureRunning(context: String) async -> Bool
-    func run(arguments: [String], context: String, captureOutput: Bool) async -> BetterDisplayExecutionResult?
+    func ensureRunning(context: String) async -> BetterDisplayAvailability
+    func run(arguments: [String], context: String, captureOutput: Bool) async -> BetterDisplayExecutionResult
 }
